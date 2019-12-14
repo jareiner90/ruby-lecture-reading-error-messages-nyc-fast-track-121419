2cfd357e54f449c280ffd121209d46912602147d
John Reiner <johnreiner1990@gmail.com>
2019-12-14 16:33:34 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -9,9 +9,8 @@ puts "Hello #{name}!"
 names = ["John", "Carl", "Patrick"]
 
 def say_hi(ary)
-  ary.each do {|name| "Hello! #{name}"}
+  ary.each {|name| "Hello! #{name}"}
   end
-
 end
 
 say_hi(names)
